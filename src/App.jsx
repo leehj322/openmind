cdd8e29be106d2666b0aa8ea535db515e662a7d2
Minchan <98478661+itscold96@users.noreply.github.com>
2024-07-14 00:38:32 +0900

    env: theme provider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
 import PageRouter from './PageRouter';
 import GlobalStyle from './styles/@shared/GlobalStyles';
+import theme from './styles/@shared/theme';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle>
-        <PageRouter />
+        <ThemeProvider theme={theme}>
+          <PageRouter />
+        </ThemeProvider>
       </GlobalStyle>
     </QueryClientProvider>
   );
