bed38e1460329296d64f22a2c205d0c036cde86b
Minchan <98478661+itscold96@users.noreply.github.com>
2024-07-14 00:35:34 +0900

    env: 공통 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PageRouter from './PageRouter';
+import GlobalStyle from './styles/@shared/GlobalStyles';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <PageRouter />
+      <GlobalStyle>
+        <PageRouter />
+      </GlobalStyle>
     </QueryClientProvider>
   );
 }
---
+++ src/styles/@shared/theme.js
@@ -0,0 +1,46 @@
+const windowSize = {
+  desktop: 'screen and (min-width: 1200px)',
+  tablet: 'screen and (min-width: 768px) and (max-width: 1199px)',
+  mobile: 'screen and (min-width: 375px) and (max-width: 767px)',
+};
+
+const fontSize = {
+  large: '20px',
+  medium: '18px',
+  small: '16px',
+  xsmall: '14px',
+};
+
+const fontWeight = {
+  regular: '400',
+  medium: '500',
+  bold: '700',
+};
+
+const light = {
+  // background: '#fff',
+  // fontPrimary: 'black',
+  // fontSecondary: 'gray',
+  // primary: '#00a0ff',
+  // secondary: '#ddd',
+  // hover: '#00a0ff50',
+};
+
+const dark = {
+  // background: '#fff',
+  // fontPrimary: 'black',
+  // fontSecondary: 'gray',
+  // primary: '#00a0ff',
+  // secondary: '#ddd',
+  // hover: '#00a0ff50',
+};
+
+const theme = {
+  windowSize,
+  fontSize,
+  fontWeight,
+  light,
+  dark,
+};
+
+export default theme;
