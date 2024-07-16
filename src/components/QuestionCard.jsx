19108642c390a8b8eaf1a4c78b460ad62490815c
Choi Young Sun <sunn7931@naver.com>
2024-07-17 04:03:00 +0900

    rename(VJNP-85): 스타일드 컴포넌트 이름 수정

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,28 +2,28 @@ import styled from 'styled-components';
 import dummyCat from '../assets/images/cute_cat_img.png';
 import receivedQuestionIcon from '../assets/images/messages_icon.png';
 
-const QuestionCardContainer = styled.section`
+const StyledQuestionCardContainer = styled.section`
   min-width: 186px;
-  width: 220px;
+  width: 100%;
   height: 187px;
   border-radius: 16px;
   border: 1px solid var(--gray40);
   padding: 20px;
 `;
 
-const ProfileImg = styled.img`
+const StyledProfileImg = styled.img`
   width: 60px;
   border-radius: 9999px;
 `;
 
-const UserName = styled.h1`
+const StyledUserName = styled.h1`
   font-size: 20px;
   font-weight: 400;
   line-height: 25px;
   margin: 10px 0;
 `;
 
-const ReceivedQuestionArea = styled.div`
+const StyledReceivedQuestionArea = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 20%;
@@ -32,13 +32,13 @@ const ReceivedQuestionArea = styled.div`
   }
 `;
 
-const ReceivedQuestionIcon = styled.img`
+const StyledReceivedQuestionIcon = styled.img`
   width: 18px;
   filter: brightness(0) saturate(100%) invert(59%) sepia(16%) saturate(0%) hue-rotate(213deg) brightness(83%)
     contrast(98%);
 `;
 
-const ReceivedQuestionText = styled.p`
+const StyledReceivedQuestionText = styled.p`
   font-size: 16px;
   font-weight: 400;
   line-height: 22px;
@@ -47,17 +47,17 @@ const ReceivedQuestionText = styled.p`
 
 function QuestionCard() {
   return (
-    <QuestionCardContainer>
-      <ProfileImg src={dummyCat} alt="답변자 프로필 사진" />
-      <UserName>이름</UserName>
-      <ReceivedQuestionArea>
+    <StyledQuestionCardContainer>
+      <StyledProfileImg src={dummyCat} alt="답변자 프로필 사진" />
+      <StyledUserName>이름</StyledUserName>
+      <StyledReceivedQuestionArea>
         <div>
-          <ReceivedQuestionIcon src={receivedQuestionIcon} alt="받은 질문 아이콘" />
-          <ReceivedQuestionText>받은 질문</ReceivedQuestionText>
+          <StyledReceivedQuestionIcon src={receivedQuestionIcon} alt="받은 질문 아이콘" />
+          <StyledReceivedQuestionText>받은 질문</StyledReceivedQuestionText>
         </div>
-        <ReceivedQuestionText>n개</ReceivedQuestionText>
-      </ReceivedQuestionArea>
-    </QuestionCardContainer>
+        <StyledReceivedQuestionText>n개</StyledReceivedQuestionText>
+      </StyledReceivedQuestionArea>
+    </StyledQuestionCardContainer>
   );
 }
 
