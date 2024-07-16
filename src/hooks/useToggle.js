ca9d60db17de2a81f2f4a6d79c6287ae408a0e37
leehj322 <gudwns8956@naver.com>
2024-07-16 12:38:36 +0900

    comment(VJNP-57): Add JSDOC on useToggle

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+/**
+ * Boolean 형태의 state를 toggle하는데 사용하는 custom hook 입니다.
+ * @param {boolean} initialValue : 초기값 (false, true)
+ * @returns [state, toggleState] : state와 state를 toggle하는 함수를 반환합니다.
+ */
 function useToggle(initialValue = false) {
   const [state, setState] = useState(initialValue);
 
