2b1a2e21af59fdbbfd634497e1b91b1252bdbba2
kzlsk <rlaflsk4887@gmail.com>
2024-07-16 22:18:47 +0900

    env : theme.js 파일 원래대로 돌림

--- src/styles/@shared/theme.js
+++ src/styles/@shared/theme.js
@@ -17,35 +17,33 @@ const fontWeight = {
   bold: '700',
 };
 
-export const light = {
+const light = {
   background: 'var(--gray20)',
   fontPrimary: 'var(--gray60)',
-  fontSecondary: 'var(--brown40)',
+  fontSecondary: 'var(--gray10)',
   hoverBlue: 'var(--blue)',
-  buttonPrimary: 'var(--brown10)',
-  //buttonSecondary: 'var(--brown20)',
-  buttonPrimaryPressed: 'var(--brown20)',
-  //buttonSecondaryPressed: 'var(--brown20)',
+  buttonPrimary: 'var(--brown40)',
+  buttonSecondary: 'var(--brown10)',
+  buttonPrimaryPressed: 'var(--brown50)',
+  buttonSecondaryPressed: 'var(--brown20)',
   borderBrown: 'var(--brown40)',
-  //BorderGray: 'var(--gray40)',
+  borderGray: 'var(--gray40)',
   buttonInactiveOpacity: '0.4',
-  buttonInactive: 'var(--brown30)',
 };
 
 // light랑 똑같이 두었는데, 구현 시에 색상만 바꾸면 됨
-export const dark = {
+const dark = {
   background: 'var(--gray20)',
   fontPrimary: 'var(--gray60)',
-  fontSecondary: 'var(gray10)',
+  fontSecondary: 'var(--gray10)',
   hoverBlue: 'var(--blue)',
   buttonPrimary: 'var(--brown40)',
-  //buttonSecondary: 'var(--brown10)',
+  buttonSecondary: 'var(--brown10)',
   buttonPrimaryPressed: 'var(--brown50)',
-  //buttonSecondaryPressed: 'var(--brown20)',
-  BorderBrown: 'var(--brown40)',
-  //BorderGray: 'var(--gray40)',
-  ButtonInactiveOpacity: '0.4',
-  ButtonInactive: 'var(--brown30)',
+  buttonSecondaryPressed: 'var(--brown20)',
+  borderBrown: 'var(--brown40)',
+  borderGray: 'var(--gray40)',
+  buttonInactiveOpacity: '0.4',
 };
 
 const settings = {
