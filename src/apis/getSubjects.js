62cc4c72f61e27c0be4681e872b149b481fd3d9a
leehj322 <gudwns8956@naver.com>
2024-07-17 12:06:01 +0900

    refactor(VJNP-57): Refactor getSubject.js
    - JSDOC 추가
    - try catch로 error 처리

--- src/apis/getSubjects.js
+++ src/apis/getSubjects.js
@@ -1,10 +1,30 @@
 import { axiosInstance } from './axiosSetup';
 import { SUBJECTS_QUERY_KEY } from '../constants/queryKeys';
 
+/**
+ * subject 목록을 GET 요청 전송해서 body값을 return하는 함수
+ * @param {number} limit 총 가져올 데이터의 양
+ * @param {number} offset 이전에 가져온 데이터의 양
+ * @param {number} sort 정렬 방식 이름순('name'), 최신순('time')
+ * @returns response의 body값을 그대로 전달
+ */
 const getSubjects = async ({ limit, offset, sort }) => {
   const query = `${SUBJECTS_QUERY_KEY}/?limit=${limit}&offset=${offset}&sort=${sort}`;
-  const { data } = await axiosInstance.get(query);
-  return data;
+
+  try {
+    const response = await axiosInstance.get(query);
+
+    // 기존의 경우 status가 200~299이면 response.ok가 날라왔는데
+    // 이번 response에는 ok가 무조건 undefined로 날라옴
+    // if (response.ok) {
+    //   console.log(response.status);
+    // }
+
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getSubjects;
