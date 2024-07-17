2dd6155f982abd7240dba6ac0307b78c46d201ad
Choi Young Sun <sunn7931@naver.com>
2024-07-17 22:58:25 +0900

    fix(VJNP-85): 세션 스토리지 추가 기능 수정

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 import receivedQuestionIcon from '../assets/images/messages_icon.png';
 import filter from '../styles/@shared/filter';
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const StyledQuestionCardContainer = styled.li`
   min-width: 186px;
@@ -12,6 +11,9 @@ const StyledQuestionCardContainer = styled.li`
   border: 1px solid var(--gray40);
   padding: 20px;
   background-color: var(--gray10);
+  &:hover {
+    cursor: pointer;
+  }
   @media (max-width: 767px) {
     height: 168px;
     padding: 16px;
@@ -75,41 +77,35 @@ const StyledReceivedQuestionText = styled.p`
 `;
 
 function SubjectCard({ subject }) {
-  useEffect(() => {
-    if (subject) {
-      // 세션 스토리지에서 기존 프로필 배열 가져오기
-      const profiles = JSON.parse(sessionStorage.getItem('profiles')) || [];
+  const navigate = useNavigate();
 
-      // 새로운 프로필 추가
-      profiles.push({
+  const handleClick = () => {
+    if (subject) {
+      const profile = {
         id: subject.id,
         name: subject.name,
         imageSource: subject.imageSource,
         questionCount: subject.questionCount,
         createdAt: subject.createdAt,
-      });
+      };
 
-      // Set을 이용해 중복 제거
-      const uniqueProfiles = Array.from(new Set(profiles.map(p => p.id))).map(id => profiles.find(p => p.id === id));
-      // 세션 스토리지에 업데이트된 프로필 배열 저장
-      sessionStorage.setItem('profiles', JSON.stringify(uniqueProfiles));
+      sessionStorage.setItem('profile', JSON.stringify(profile));
+      navigate(`/post/${subject.id}`);
     }
-  }, [subject]);
+  };
 
   return (
-    <Link to={`/post/${subject.id}`}>
-      <StyledQuestionCardContainer>
-        <StyledProfileImg src={subject.imageSource} alt="답변자 프로필 사진" />
-        <StyledUserName>{subject.name}</StyledUserName>
-        <StyledReceivedQuestionArea>
-          <div>
-            <StyledReceivedQuestionIcon src={receivedQuestionIcon} alt="받은 질문 아이콘" />
-            <StyledReceivedQuestionText>받은 질문</StyledReceivedQuestionText>
-          </div>
-          <StyledReceivedQuestionText>{subject.questionCount}개</StyledReceivedQuestionText>
-        </StyledReceivedQuestionArea>
-      </StyledQuestionCardContainer>
-    </Link>
+    <StyledQuestionCardContainer onClick={handleClick}>
+      <StyledProfileImg src={subject.imageSource} alt="답변자 프로필 사진" />
+      <StyledUserName>{subject.name}</StyledUserName>
+      <StyledReceivedQuestionArea>
+        <div>
+          <StyledReceivedQuestionIcon src={receivedQuestionIcon} alt="받은 질문 아이콘" />
+          <StyledReceivedQuestionText>받은 질문</StyledReceivedQuestionText>
+        </div>
+        <StyledReceivedQuestionText>{subject.questionCount}개</StyledReceivedQuestionText>
+      </StyledReceivedQuestionArea>
+    </StyledQuestionCardContainer>
   );
 }
 
