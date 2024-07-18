9843797cfc965de8dcaba8ef69b9df69e62cd637
kzlsk <rlaflsk4887@gmail.com>
2024-07-19 00:15:14 +0900

    feature : modal 컴포넌트 구현 (미완성)

--- src/components/feed/questionFeed/ModalComponent.jsx
+++ src/components/feed/questionFeed/ModalComponent.jsx
@@ -1,30 +1,93 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
 import styled from 'styled-components';
+import messageIcon from '../../../assets/images/message-icon.png';
+import Profile from '../../../assets/images/samples/profile-sample.png';
+import SendQuestionBtn from '../../@shared/Button';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+const StyledModal = styled(Modal)`
+  width: 612px;
+  height: 420px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--gray10);
+  border-radius: 24px;
+  box-shadow: var(--shadow3pt);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledModalHeader = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+  border-bottom: 0.5px solid var(--gray30);
+  height: 50px;
+`;
+
+const StyledModalTitleWrapper = styled.div`
+  margin: 15px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const StyledModalTitle = styled.h2`
+  font-size: 18px;
+`;
+
+const StyledModalIcon = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+
+const StyledCloseBtn = styled.button`
+  font-size: 28px;
+  cursor: pointer;
+  color: #f5a9a9;
+  font-weight: 400;
+  margin-right: 15px;
+`;
+
+const StyledModalContent = styled.main`
+  margin: 12px 15px;
+`;
+
+const StyledUserInfo = styled.div`
+  padding-left: 5px;
+  display: flex;
+  align-items: center;
+  gap: 7px;
+  font-size: 23px;
+`;
 
 const StyledProfileImg = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 35px;
+  height: 35px;
   border-radius: 50%;
 `;
 
 const StyledUserName = styled.h2`
-  margin-top: 10px;
-  font-size: 1.5em;
+  margin-left: 5px;
+  font-size: 20px;
 `;
 
-function ModalComponent({ imageSource, name }) {
+const StyledTextArea = styled.textarea`
+  margin-top: 15px;
+  width: 558px;
+  height: 220px;
+  background-color: var(--brown10);
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+function ModalComponent({ name }) {
+  Modal.setAppElement('#root');
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const showModal = () => {
@@ -37,11 +100,33 @@ function ModalComponent({ imageSource, name }) {
   return (
     <div>
       <button onClick={showModal}>모달 열기</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
-        <StyledProfileImg src={imageSource} alt="프로필" />
-        <StyledUserName>{name}</StyledUserName>
-        <button onClick={closeModal}>닫기</button>
-      </Modal>
+
+      <StyledModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <StyledModalHeader>
+          <StyledModalTitleWrapper>
+            <StyledModalIcon src={messageIcon} alt="말풍선" />
+            <StyledModalTitle>질문을 작성하세요!</StyledModalTitle>
+          </StyledModalTitleWrapper>
+          <StyledCloseBtn onClick={closeModal}>X</StyledCloseBtn>
+        </StyledModalHeader>
+        <StyledModalContent>
+          <StyledUserInfo>
+            To.
+            <StyledProfileImg src={Profile} />
+            <StyledUserName>{name}</StyledUserName>
+          </StyledUserInfo>
+          <StyledTextArea placeholder="질문을 입력해주세요!"></StyledTextArea>
+        </StyledModalContent>
+        <SendQuestionBtn
+          type="submit"
+          pagePath="/post/123"
+          disabled={false}
+          width="558px"
+          height="50px"
+          style={{ backgroundColor: 'var(--brown20)', color: 'var(--gray40)' }}>
+          질문 보내기
+        </SendQuestionBtn>
+      </StyledModal>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,9 @@
-import styled from 'styled-components';
-import Button from '../components/@shared/Button';
-import SoftArrow from '../assets/images/softarrow.png';
-import DeepArrow from '../assets/images/deeparrow.png';
-import React, { useState } from 'react';
-
-const ButtonContainer = styled.div`
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
+import ReactModal from 'react-modal';
+import ModalComponent from '../components/feed/questionFeed/ModalComponent';
+import profile from '../assets/images/samples/profile-sample.png';
 
 function Home() {
-  return (
-    <ButtonContainer>
-      <Button pagePath="/post/123" width="504px" height="100px" imgSrc={SoftArrow}>
-        질문하러 가기
-      </Button>
-    </ButtonContainer>
-  );
+  return <ModalComponent imageSource={profile} name="go5rae" />;
 }
 
 export default Home;
