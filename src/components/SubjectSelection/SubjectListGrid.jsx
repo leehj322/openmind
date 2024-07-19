26a251ab8223bfdd7bf48eaff101bc55777944e5
leehj322 <gudwns8956@naver.com>
2024-07-19 10:51:08 +0900

    comment(VJNP-60): 코멘트 간단한 수정

--- src/components/SubjectSelection/SubjectListGrid.jsx
+++ src/components/SubjectSelection/SubjectListGrid.jsx
@@ -27,40 +27,39 @@ function SubjectListGrid({ sortBy }) {
   const windowSize = useWindowSize();
   const [currentPage, setCurrentPage] = useState(1);
   const [pageNumList, setPageNumList] = useState([1, 2, 3, 4, 5]);
-  // 화살표 버튼이 보일지 안보일지 설정
-  // both: 둘 다 보임, left: 왼쪽만 보임, right: 오른쪽만 보임, none: 둘다 안보임
+  // 어떤 화살표 버튼이 보일지 설정 (both, left, right, none);
   const [arrowBtnVisible, setArrowBtnVisible] = useState('right');
 
-  // 현재 페이지의 grid 형태에 따라 limit값 수정 및
-  // currentPage 값에 따라서 offset 계산 후 GET 요청 전송
+  // 현재 페이지의 grid 형태에 따라 limit, offset값 수정 후 GET요청 전송
   const limit = windowSize.width > GRID_BREAKPOINT.tablet ? NUM_OF_SUBJECTS.pc : NUM_OF_SUBJECTS.other;
   const offset = limit * (currentPage - 1);
   const { data } = useSubjectsQuery(limit, offset, sortBy);
   const { count, results } = data;
 
-  // 필요한 전체 페이지 수 계산
-  // 해당 값이 5페이지 미만이면 기존 pageNumList 값인 [1, 2, 3, 4, 5]에서 필요한 만큼만 slice
+  // 필요한 전체 페이지 수 (totalPageCount) 값 계산
   const totalPageCount = Math.ceil(count / limit);
   useEffect(() => {
+    // totalPageCount값이 5페이지 미만이면 기존 pageNumList 값인 [1, 2, 3, 4, 5]에서 필요한 만큼만 slice
     if (totalPageCount < 5) {
       setPageNumList(prevPageNumList => prevPageNumList.slice(0, totalPageCount));
     }
   }, [totalPageCount]);
 
-  // 화살표 버튼 visible 옵션 제어
-  const isFirstPageNumList = pageNumList[0] === 1;
-  const isLastPageNumList = totalPageCount === pageNumList[pageNumList.length - 1];
+  // 화살표 보이기 상태 제어
   useEffect(() => {
     if (totalPageCount <= 1) {
+      // 전체 페이지가 1페이지만 있거나 없는 경우 화살표가 양쪽 다 안보임
       setArrowBtnVisible('none');
     } else if (currentPage === 1) {
+      // 현재 페이지가 첫 페이지인 경우 왼쪽 화살표 안보임
       setArrowBtnVisible('right');
     } else if (currentPage === totalPageCount) {
+      // 현재 페이지가 마지막 페이지인 경우 오른쪽 화살표 안보임
       setArrowBtnVisible('left');
     } else {
       setArrowBtnVisible('both');
     }
-  }, [pageNumList, currentPage]);
+  }, [totalPageCount, pageNumList, currentPage]);
 
   /**
    * 페이지 네이션 네비게이터의 버튼을 클릭했을때의 동작 핸들러
@@ -83,7 +82,7 @@ function SubjectListGrid({ sortBy }) {
      * 누른 버튼이 숫자 버튼인 경우의 동작
      */
     const handleNumBtnClick = () => {
-      // currentPage 업데이트
+      // 현재 페이지 최신화
       const nextCurrentPage = Number(targetText);
       setCurrentPage(nextCurrentPage);
 
@@ -113,7 +112,7 @@ function SubjectListGrid({ sortBy }) {
      * 누른 버튼이 화살표 버튼인 경우 동작
      */
     const handleArrowBtnClick = () => {
-      // 현재 페이지가 첫 페이지가 아니면 currentPage - 1
+      // 왼쪽 이동 동작 : 현재 페이지가 첫 페이지가 아니면 currentPage - 1
       if (targetText === '<' && currentPage !== 1) {
         const nextCurrentPage = currentPage - 1;
         setCurrentPage(nextCurrentPage);
@@ -121,11 +120,12 @@ function SubjectListGrid({ sortBy }) {
         // currentPage가 마지막 끝 3번째 페이지 이하
         // !isFirstPageNumList : 처음 3번째 페이지가 아닌 경우
         // 왼쪽 버튼 눌렀을때 pageNumList값이 1씩 감소해야 함
+        const isFirstPageNumList = pageNumList[0] === 1;
         if (!isFirstPageNumList && currentPage <= totalPageCount - 2) {
           setPageNumList(prevPageNumList => prevPageNumList.map(pageNum => pageNum - 1));
         }
       }
-      // 현재 페이지가 마지막 페이지가 아니면 currentPage + 1
+      // 오른쪽 이동 동작 : 현재 페이지가 마지막 페이지가 아니면 currentPage + 1
       if (targetText === '>' && currentPage !== totalPageCount) {
         const nextCurrentPage = currentPage + 1;
         setCurrentPage(nextCurrentPage);
@@ -133,6 +133,7 @@ function SubjectListGrid({ sortBy }) {
         // currentPage가 3페이지이상
         // !isLastPageNumList : 끝에서 3페이지가 아닌 경우
         // 오른쪽버튼 눌렀을때 pageNumList값이 1씩 증가해야 함
+        const isLastPageNumList = totalPageCount === pageNumList[pageNumList.length - 1];
         if (!isLastPageNumList && currentPage >= 3) {
           setPageNumList(prevPageNumList => prevPageNumList.map(pageNum => pageNum + 1));
         }
