c9c4ede6c4391c8432284c8b1ee29b1195a703dc
Choi Young Sun <sunn7931@naver.com>
2024-07-20 17:53:56 +0900

    fix(VJNP-87): 트위터 메타태그 수정

--- src/components/sns/TwitterShareButton.jsx
+++ src/components/sns/TwitterShareButton.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import TwitterIcon from '../../images/twitter_icon.png';
 import { Helmet } from 'react-helmet';
-import { useState } from 'react';
 import Tooltip from './Tooltip';
 
 const StyledShareIcon = styled.img`
@@ -11,39 +10,23 @@ const StyledShareIcon = styled.img`
 `;
 
 function TwitterShareButton({ subject }) {
-  const [helmetData, setHelmetData] = useState({
-    title: '',
-    description: '',
-    image: '',
-  });
-
   const shareTwitter = () => {
     const sendText = `${subject.name}님에게 궁금한 점을 물어보러 가요!`;
     const sendUrl = 'http://localhost:5173/post/123'; //props로 뒷부분 아이디 받아와서 쓸 수 있도록 제작 예정
-
-    //오류가 생겨도 undefined가 표출되지 않도록 처리
-    setHelmetData({
-      title: subject.name ? `${subject.name}님의 피드` : '피드',
-      description: subject.name ? `${subject.name}님에게 궁금한 점을 물어보러 가요!` : '궁금한 점을 물어보러 가요!',
-      image: subject.imageSource || '',
-    });
 
     window.open('https://twitter.com/intent/tweet?text=' + sendText + '&url=' + sendUrl);
   };
 
   return (
     <>
-      {helmetData && (
-        <Helmet>
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={helmetData.title} />
-          <meta name="twitter:description" content={helmetData.description} />
-          <meta name="twitter:image" content={helmetData.image} />
-        </Helmet>
-      )}
+      <Helmet>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="피드 구경하러 가기" />
+        <meta name="twitter:image" content="무엇이든 물어보세요!" />
+      </Helmet>
 
       <Tooltip content="트위터로 공유하기">
-        <StyledShareIcon src={TwitterIcon} alt="클립보드 복사 아이콘" onClick={shareTwitter} />
+        <StyledShareIcon src={TwitterIcon} alt="트위터 아이콘" onClick={shareTwitter} />
       </Tooltip>
     </>
   );
