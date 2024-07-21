ddf103e1eab236aeffe83e0fb0d5944236d78fbf
leehj322 <gudwns8956@naver.com>
2024-07-21 17:30:54 +0900

    refactor(VJNP-103): 공통 컴포넌트 및 하이퍼링크 적용

--- src/components/SubjectSelection/Header.jsx
+++ src/components/SubjectSelection/Header.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
-import LightBrownButton from '../@shared/LightBrownButton';
 import LogoImgUrl from '../../assets/images/logo.svg';
+import Button from '../@shared/Button';
 import { MEDIA_QUERY_SIZES } from '../../constants/mediaQuerySizes';
+import { Link } from 'react-router-dom';
+import rightArrowImg from '../../assets/images/softarrow.png';
 
 const StyledHeaderArea = styled.header`
   display: flex;
@@ -24,11 +26,35 @@ const StyledLogoImg = styled.img`
   height: 57px;
 `;
 
+const StyledButtonWrapper = styled.div`
+  & button {
+    border: 1px solid ${({ theme }) => theme.brown40};
+
+    @media ${MEDIA_QUERY_SIZES.mobile} {
+      width: 127px;
+      height: 34px;
+      font-size: 14px;
+      gap: 4px;
+      padding: 8px 12px;
+    }
+  }
+`;
+
 function Header() {
+  const userSubjectId = 0; // 로컬스토리지 접근 값 사용
+  // const userSubjectId = window.localStorage.getItem(key);
+  const buttonNextPath = userSubjectId ? `/post/${userSubjectId}/answer` : '/';
+
   return (
     <StyledHeaderArea>
-      <StyledLogoImg src={LogoImgUrl} />
-      <LightBrownButton>답변하러 가기</LightBrownButton>
+      <Link to="/">
+        <StyledLogoImg src={LogoImgUrl} />
+      </Link>
+      <StyledButtonWrapper>
+        <Button imgSrc={rightArrowImg} pagePath={buttonNextPath}>
+          답변하러 가기
+        </Button>
+      </StyledButtonWrapper>
     </StyledHeaderArea>
   );
 }
