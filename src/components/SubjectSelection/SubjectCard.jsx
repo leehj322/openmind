ec0a6886809d158895491ee913456cba7ade3bac
leehj322 <gudwns8956@naver.com>
2024-07-23 12:35:28 +0900

    refactor(VJNP-53): 전체 수정 반영

--- src/components/SubjectSelection/SubjectCard.jsx
+++ src/components/SubjectSelection/SubjectCard.jsx
@@ -1,18 +1,33 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import receivedQuestionIcon from '../../assets/images/messages_icon.png';
-import filter from '../../styles/@shared/filter';
 import { useNavigate } from 'react-router-dom';
 
+const waveAnimation = keyframes`
+  0% {
+    transform: translateY(0%);
+  }
+  50% {
+    transform: translateY(-5%);
+  }
+  100% {
+    transform: translateY(0%);
+  }
+`;
+
 const StyledQuestionCardContainer = styled.li`
-  min-width: 186px;
   width: 100%;
+  min-width: 112px;
   height: 187px;
   border-radius: 16px;
-  border: 1px solid var(--gray40);
+  border: 1px solid ${props => props.theme.gray40};
   padding: 20px;
-  background-color: var(--gray10);
+  background-color: ${props => props.theme.gray10};
+  transition: transform 1s ease-in-out;
+
   &:hover {
     cursor: pointer;
+    position: relative;
+    animation: ${waveAnimation} 1s infinite;
   }
   @media (max-width: 767px) {
     height: 168px;
@@ -35,6 +50,9 @@ const StyledUserName = styled.h1`
   font-weight: 400;
   line-height: 25px;
   margin: 12px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
   @media (max-width: 767px) {
     font-size: 18px;
   }
@@ -59,7 +77,7 @@ const StyledReceivedQuestionIcon = styled.img`
   width: 18px;
   height: 18px;
   margin-right: 4px;
-  filter: ${filter.gray50};
+  filter: ${props => props.theme.gray50Filter};
   @media (max-width: 767px) {
     width: 16px;
     height: 16px;
@@ -70,7 +88,7 @@ const StyledReceivedQuestionText = styled.p`
   font-size: 16px;
   font-weight: 400;
   line-height: 22px;
-  color: var(--gray40);
+  color: ${props => props.theme.gray40};
   @media (max-width: 767px) {
     font-size: 14px;
   }
--- src/components/SubjectSelection/SubjectList.jsx
+++ src/components/SubjectSelection/SubjectList.jsx
@@ -1,29 +1,54 @@
 import styled from 'styled-components';
-import SubjectListGrid from './SubjectListGrid';
-import { Suspense } from 'react';
+import SubjectListGridSkeleton from './SubjectListGridSkeleton';
+import { Suspense, lazy, useState, useEffect } from 'react';
+import { MEDIA_QUERY_SIZES } from '../../constants/mediaQuerySizes';
 
-const SubjectListArea = styled.section`
+const StyledSubjectListArea = styled.section`
   margin: 0 auto 40px;
   width: 940px;
 
-  @media screen and (min-width: 768px) and (max-width: 1199px) {
+  @media ${MEDIA_QUERY_SIZES.tablet} {
     margin: 0 32px 40px;
     width: auto;
   }
 
-  @media screen and (min-width: 375px) and (max-width: 767px) {
+  @media ${MEDIA_QUERY_SIZES.mobile} {
     margin: 0 24px 60px;
     width: auto;
   }
 `;
 
+const SubjectListGrid = lazy(() => import('./SubjectListGrid'));
+
+/**
+ * 로딩 시간이 WAIT_TIME(단위: ms) 이상인 경우 skeleton UI를 보여주고 아닌 경우에 보여주지 않는 컴포넌트
+ * @returns null 또는 Skeleton UI를 반환합니다.
+ */
+function DeferredComponent() {
+  const [isDeferred, setIsDeferred] = useState(false);
+  const WAIT_TIME = 200;
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setIsDeferred(true);
+    }, WAIT_TIME);
+    return () => clearInterval(timeOut);
+  }, []);
+
+  if (!isDeferred) {
+    return null;
+  }
+
+  return <SubjectListGridSkeleton />;
+}
+
 function SubjectList({ sortBy }) {
   return (
-    <SubjectListArea>
-      <Suspense fallback={<div>Loading...</div>}>
+    <StyledSubjectListArea>
+      <Suspense fallback={<DeferredComponent />}>
         <SubjectListGrid sortBy={sortBy} />
       </Suspense>
-    </SubjectListArea>
+    </StyledSubjectListArea>
   );
 }
 
